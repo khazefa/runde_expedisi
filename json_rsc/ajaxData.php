3e87a015a1fb10cd9df94ebca186d7449029e4c7
khazefion <cybergitt@gmail.com>
2018-08-11 21:38:36 +0700

    Add ajax data parser

---
+++ json_rsc/ajaxData.php
@@ -0,0 +1,20 @@
+<?php
+require("../includes/constants.php");
+require('../includes/class.db.php');
+$database = DB::getInstance();
+require("../includes/global_helper.php");
+require("../includes/common_helper.php");
+
+$getact = htmlspecialchars($_GET["act"], ENT_QUOTES, 'UTF-8');
+
+if ($getact == "get_kabupaten"){
+    $key = htmlspecialchars($_GET["key"], ENT_QUOTES, 'UTF-8');
+    //Fetch all state data
+    $query = "SELECT * FROM kabupaten WHERE id_provinsi = ".$key."";
+    $results = $database->get_results( $query );
+    foreach( $results as $row )
+    {
+        echo "<option value='$row[id_kabupaten]'>$row[nama_kabupaten]</option>";
+    }
+}
+?>
